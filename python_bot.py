9394df2ad6052533ce341cf589ff587a3538f43f
tony.joblin <tonyjoblin@yahoo.com.au>
2017-03-12 16:24:33 +1000

    Implement robot state class, move and place command handlers

--- python_bot.py
+++ python_bot.py
@@ -4,26 +4,97 @@ STATE_START = 0
 STATE_PLACED = 1
 STATE_EXIT = 2
 
+class Robot:        
+    def __init__(self, state = None, x = None, y = None, facing = None):
+        self.state = state
+        if self.state is None:
+            self.state = STATE_START
+        self.x = x
+        self.y = y
+        self.facing = facing
+
+def copy_robot(robot):
+    new_robot = Robot(robot.state, robot.x, robot.y, robot.facing)
+    return new_robot
+
+def is_valid_state(robot):
+    if robot.state == STATE_PLACED:
+        if robot.x < 0:
+            return False
+        if robot.y < 0:
+            return False
+        if robot.x > 4:
+            return False
+        if robot.y > 4:
+            return False
+    return True
+
+def handle_place(initial_state, args, outputs):
+    next_state = copy_robot(initial_state)
+    try:
+        arg_list = args.split(',')
+        x = int(arg_list[0])
+        y = int(arg_list[1])
+        facing = arg_list[2].strip()
+        next_state.state = STATE_PLACED
+        next_state.x = x
+        next_state.y = y
+        next_state.facing = facing
+        if not is_valid_state(next_state):
+            return initial_state
+    except:
+        pass
+    return next_state
+
+def handle_move(initial_state, args, outputs):
+    if initial_state.state != STATE_PLACED:
+        return initial_state
+
+    next_state = copy_robot(initial_state)
+    if next_state.facing == 'north':
+        next_state.y += 1
+    if next_state.facing == 'south':
+        next_state.y -= 1
+    if next_state.facing == 'east':
+        next_state.x += 1
+    if next_state.facing == 'west':
+        next_state.x -= 1
+    
+    if not is_valid_state(next_state):
+        return initial_state
+    return next_state
+
 def get_command_and_args(input):
     input = input.strip()
     pos = input.find(' ')
     if pos == -1:
-        return (input, None)
+        return input, None
     command = input[:pos]
-    args = input[pos:]
-    return (command, args)
+    args = input[pos + 1:]
+    return command, args
 
 def robot_controller(initial_state, input, outputs = None):
     command, args = get_command_and_args(input)
-    if command == 'exit':
-        return (STATE_EXIT,)
-    return initial_state
+    next_state = initial_state
+    if initial_state.state == STATE_START:
+        if command == 'exit':
+            next_state = Robot(STATE_EXIT)
+        elif command == 'place':
+            next_state = handle_place(initial_state, args, outputs)
+    elif initial_state.state == STATE_PLACED:
+        if command == 'exit':
+            next_state = Robot(STATE_EXIT)
+        elif command == 'place':
+            next_state = handle_place(initial_state, args, outputs)
+        elif command == 'move':
+            next_state = handle_move(initial_state, args, outputs)
+    return next_state
 
 def robot_command_loop(inputs, outputs):
-    state = (STATE_START,)
+    state = Robot()
     for command in inputs:
         next_state = robot_controller(state, command, outputs)
-        if next_state[0] == STATE_EXIT: # exit state
+        if next_state.state == STATE_EXIT: # exit state
             break
         state = next_state
 
--- python_bot_unittests.py
+++ python_bot_unittests.py
@@ -1,5 +1,6 @@
 import unittest
 import python_bot
+from python_bot import *
 
 class get_command_and_args_tests(unittest.TestCase):
 
@@ -9,13 +10,145 @@ class get_command_and_args_tests(unittest.TestCase):
     def test_trims_white_space(self):
         self.assertEqual(python_bot.get_command_and_args(' exit '), ('exit', None))
 
+    def test_returns_args(self):
+        self.assertEqual(
+            python_bot.get_command_and_args('place 1,2,north'),
+            ('place','1,2,north')
+            )
+
 class robot_controller_tests(unittest.TestCase):
 
     def test_enters_exit_state_on_exit_cmd(self):
-        initial_state = (python_bot.STATE_START)
-        next_state = python_bot.robot_controller(initial_state, 'exit')
-        self.assertEqual(python_bot.STATE_EXIT, next_state[0])
+        initial_state = Robot()
+        
+        next_state = robot_controller(initial_state, 'exit')
+        
+        self.assertEqual(STATE_EXIT, next_state.state)
 
+    def test_valid_place_puts_robot_on_table(self):
+        initial_state = Robot()
+        
+        next_state = robot_controller(initial_state, 'place 1,2,north')
+        
+        self.assertEqual(STATE_PLACED, next_state.state)
+        self.assertEqual(1, next_state.x)
+        self.assertEqual(2, next_state.y)
+        self.assertEqual('north', next_state.facing)
+
+    def test_cannot_place_robot_off_table(self):
+        initial_state = Robot()
+        
+        next_state = robot_controller(initial_state, 'place -1,2,north')
+        
+        self.assertEqual(STATE_START, next_state.state)
+        
+
+    def test_cannot_take_robot_off_table(self):
+        initial_state = Robot(STATE_PLACED, 1, 2, 'north')
+        
+        next_state = robot_controller(initial_state, 'place -1,2,north')
+        
+        self.assertEqual(STATE_PLACED, next_state.state)
+        self.assertEqual(1, next_state.x)
+        self.assertEqual(2, next_state.y)
+        self.assertEqual('north', next_state.facing)
+
+    def test_place_args_may_contain_ws(self):
+        initial_state = Robot()
+        
+        next_state = robot_controller(initial_state, 'place 1 , 2,  north')
+        
+        self.assertEqual(STATE_PLACED, next_state.state)
+        self.assertEqual(1, next_state.x)
+        self.assertEqual(2, next_state.y)
+        self.assertEqual('north', next_state.facing)
+
+    def test_move_north(self):
+        initial_state = Robot(STATE_PLACED, 1, 2, 'north')
+        
+        next_state = robot_controller(initial_state, 'move')
+        
+        self.assertEqual(STATE_PLACED, next_state.state)
+        self.assertEqual(1, next_state.x)
+        self.assertEqual(3, next_state.y)
+        self.assertEqual('north', next_state.facing)
+
+    def test_move_south(self):
+        initial_state = Robot(STATE_PLACED, 1, 2, 'south')
+        
+        next_state = robot_controller(initial_state, 'move')
+        
+        self.assertEqual(STATE_PLACED, next_state.state)
+        self.assertEqual(1, next_state.x)
+        self.assertEqual(1, next_state.y)
+        self.assertEqual('south', next_state.facing)
+    
+    def test_move_east(self):
+        initial_state = Robot(STATE_PLACED, 1, 2, 'east')
+        
+        next_state = robot_controller(initial_state, 'move')
+        
+        self.assertEqual(STATE_PLACED, next_state.state)
+        self.assertEqual(2, next_state.x)
+        self.assertEqual(2, next_state.y)
+        self.assertEqual('east', next_state.facing)
+
+    def test_move_west(self):
+        initial_state = Robot(STATE_PLACED, 1, 2, 'west')
+        
+        next_state = robot_controller(initial_state, 'move')
+        
+        self.assertEqual(STATE_PLACED, next_state.state)
+        self.assertEqual(0, next_state.x)
+        self.assertEqual(2, next_state.y)
+        self.assertEqual('west', next_state.facing)
+
+    def test_cannot_move_north_off_table(self):
+        initial_state = Robot(STATE_PLACED, 1, 4, 'north')
+        
+        next_state = robot_controller(initial_state, 'move')
+        
+        self.assertEqual(STATE_PLACED, next_state.state)
+        self.assertEqual(1, next_state.x)
+        self.assertEqual(4, next_state.y)
+        self.assertEqual('north', next_state.facing)
+
+    def test_cannot_move_south_off_table(self):
+        initial_state = Robot(STATE_PLACED, 1, 0, 'south')
+        
+        next_state = robot_controller(initial_state, 'move')
+        
+        self.assertEqual(STATE_PLACED, next_state.state)
+        self.assertEqual(1, next_state.x)
+        self.assertEqual(0, next_state.y)
+        self.assertEqual('south', next_state.facing)
+    
+    def test_cannot_move_east_off_table(self):
+        initial_state = Robot(STATE_PLACED, 4, 2, 'east')
+        
+        next_state = robot_controller(initial_state, 'move')
+        
+        self.assertEqual(STATE_PLACED, next_state.state)
+        self.assertEqual(4, next_state.x)
+        self.assertEqual(2, next_state.y)
+        self.assertEqual('east', next_state.facing)
+
+    def test_cannot_move_west_off_table(self):
+        initial_state = Robot(STATE_PLACED, 0, 2, 'west')
+        
+        next_state = robot_controller(initial_state, 'move')
+        
+        self.assertEqual(STATE_PLACED, next_state.state)
+        self.assertEqual(0, next_state.x)
+        self.assertEqual(2, next_state.y)
+        self.assertEqual('west', next_state.facing)
+
+    def test_cannot_move_while_off_table(self):
+        initial_state = Robot(STATE_START)
+        
+        next_state = robot_controller(initial_state, 'move')
+        
+        self.assertEqual(STATE_START, next_state.state)
 
 def main():
     unittest.main()
